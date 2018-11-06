5a8b28cdd21199b3012c08fe1f7152a3b15ab5e5
edrflt <efouille@student.42.fr>
2018-11-06 19:41:56 +0100

    config pour le test automatisé

---
+++ my_config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+###################################
+#     Mandatory configuration     #
+###################################
+
+PATH_LIBFT=/tmp/eft/libft
+
+###################################
+#     Optionnal configuration     #
+###################################
+
+PATH_DEEPTHOUGHT=${PATH_TEST}
+
+COLOR_OK="${GREEN}"
+COLOR_FAIL="${RED}"
+COLOR_WARNING="${YELLOW}"
+COLOR_TITLE="${BOLD}${BLUE}"
+COLOR_FUNC="${CYAN}"
+COLOR_PART="${UNDERLINE}${PURPLE}"
+COLOR_TOTAL="${BOLD}${YELLOW}"
+COLOR_DEEPTHOUGHT_PATH="${BOLD}${PURPLE}"
