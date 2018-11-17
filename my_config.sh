bd65802f6ed3e31d208823276f73489eb82f3696
Edgar FOUILLET <efouille@e3r2p23.42.fr>
2018-11-17 19:16:13 +0100

    support derniere version

--- my_config.sh
+++ my_config.sh
@@ -4,7 +4,7 @@
 #     Mandatory configuration     #
 ###################################
 
-PATH_LIBFT=/tmp/eft/libft
+PATH_LIBFT=$EFT_PATH_/libft
 
 ###################################
 #     Optionnal configuration     #
